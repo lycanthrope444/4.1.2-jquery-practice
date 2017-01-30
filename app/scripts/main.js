bba884092c32e7a1a91a463ff0400b231f869539
Nathan <nathanstarwalt@gmail.com>
2017-01-30 14:55:10 -0500

    Header events created

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,32 @@
 (function(){
 
   $(function(){
+    //List of variables to be used
     var myFilesHeader = $(".files").find('h3'),
     mailHeader = $('.mail').find('h3'),
-    cloudHeader = $('.cloud').find('h3');
-    console.log(myFilesHeader);
-    console.log(mailHeader);
-    console.log(cloudHeader);
+    cloudHeader = $('.cloud').find('h3'),
+    logOutHeader = $(".log-out").find('h3'),
+    listFiles = $(".ol-files"),
+    listMail = $(".ol-mail"),
+    listCloud = $(".ol-cloud");
+
+//event handlers for headers
+    myFilesHeader.click(function(){
+      listFiles.slideToggle(400);
+    });
+
+    mailHeader.click(function(){
+      listMail.slideToggle(400);
+    });
+
+    cloudHeader.click(function(){
+      listCloud.slideToggle(400);
+    });
+
+    logOutHeader.click(function(){
+      console.log("log-out clicked");
+    });
 
   }());
-
 
 })();
